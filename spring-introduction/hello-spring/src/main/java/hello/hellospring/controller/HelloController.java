eb6414d71c279df9eec26dcb1104df2e76d2a51a
seock1000 <tjrcjs0705@gmail.com>
2023-11-24 15:56:30 +0900

    스프링 입문 - template engine 활용 컨텐츠 전달

--- spring-introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring-introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,15 +3,32 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    // 정적 컨텐츠 반환
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
         // controller에서 문자를 return 하면 viewResolver가 화면을 찾아 처리
         // templates/{viewName}+.html 로 해석
         return "hello";
+    }
+
+    // template engine 사용하여 반환
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // api 방식
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // html 양식 없이 문자 그대로 전달
     }
 }
