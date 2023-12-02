58443b197aa8c9754229397b75996b6368d34db8
seock1000 <tjrcjs0705@gmail.com>
2023-12-02 13:16:04 +0900

    스프링 핵심 원리 기본 - 할인정책 변경

--- spring-core-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements  OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 할인 정책 변경에서 클라이언트 코드에 변경이 발생!
+    // OCP, DIP 위반
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
