f4bebd6615fd49a4917468a64f063fe4d1629341
seock1000 <tjrcjs0705@gmail.com>
2023-12-02 13:40:38 +0900

    스프링 핵심 원리 기본 - AppConfig 작성 : 관심사 분리

--- spring-core-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,11 +9,13 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements  OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // 할인 정책 변경에서 클라이언트 코드에 변경이 발생!
-    // OCP, DIP 위반
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
