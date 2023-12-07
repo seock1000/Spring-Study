c6b33684f5e54556d2f49223ab905960b88b47cc
seock1000 <tjrcjs0705@gmail.com>
2023-12-07 15:07:58 +0900

    스프링 핵심 원리 기본 - 컴포넌트  스캔과 의존관계 자동주입

---
+++ spring-core-basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // AppConfig Component Scan 대상에서 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
