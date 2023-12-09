179abe0af024870f9945e936832771a2bc581f69
seock1000 <tjrcjs0705@gmail.com>
2023-12-09 14:04:52 +0900

    스프링 핵심 원리 기본 - 빈 초기화, 소멸 메서드 : InitializingBean, DisposableBean 인터페이스

---
+++ spring-core-basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    // 초기화 콜백 메서드
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    // 스프링 컨테이너 종료 전 콜백 메서드
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
