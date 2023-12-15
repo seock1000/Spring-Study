d46ef8f2488b11924297382c42d1522c67d1f67e
seock1000 <tjrcjs0705@gmail.com>
2023-12-15 19:17:40 +0900

    스프링 핵심 원리 기본 - 웹스코프

---
+++ spring-core-basic/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+// 로그 출력을 위한 MyLogger 클래스
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        // 빈 생성 시점에 random uuid를 생성하여 저장
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
+    }
+}
