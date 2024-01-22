d0d2f3130beece5debec98832d663bf2c09a94ab
seock1000 <tjrcjs0705@gmail.com>
2024-01-22 16:24:48 +0900

    실용적인 컨트롤러

---
+++ spring-web-core-1/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,47 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 애노테이션 기반의 컨트롤러는 유연하기 때문에, String으로 viewName 반환 가능 + 파라미터는 RequestParam으로 파라미터 파싱 및 형변환 가능 + Model을 파라미터로 받는 것 가능
+ */
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String save(@RequestParam("username") String username,
+                             @RequestParam("age") int age,
+                             Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
+    }
+
+}
