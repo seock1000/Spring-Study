01e5cb1b9fc3ef8b8bec903bb347f1d5677bb296
seock1000 <tjrcjs0705@gmail.com>
2024-01-27 16:47:11 +0900

    스프링 MVC 2 - 파라미터 간편한 조회

--- spring-web-core-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ spring-web-core-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -90,6 +92,37 @@ public class RequestParamController {
     @RequestMapping("/request-param-default")
     public String requestParamDefault(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        /*
+        // @ModelAttribute가 해주는 역할
+        // 값이 잘못 들어오면(ex) int age = abc) BindException
+        @RequestParam String username,
+        @RequestParam int age
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+         */
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        //log.info("helloDate={}", helloData.toString());
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략 가능 : ArgumentResolver(HttpServletRequest 등) 제외
+     * 생략 시,
+     * 단순 타입(String, Integer) 등에는 @RequestParam 자동 적용
+     * 나머지는 @ModelAttribute 자동 적용
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
