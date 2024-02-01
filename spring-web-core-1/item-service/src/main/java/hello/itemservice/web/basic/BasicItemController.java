e49bf923eb4cf68a453e352e04d968c74f36847b
seock1000 <tjrcjs0705@gmail.com>
2024-02-01 18:55:00 +0900

    스프링 MVC 1 - RedirectAttributes

--- spring-web-core-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-web-core-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -72,10 +73,19 @@ public class BasicItemController {
         return "basic/item"; // 뷰는 이게 뜨지만, 새로고침 시 post 요청 재전송
     }
 
-    @PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId(); // PRG 패턴, but 인코딩 해서 넣어야함
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        // path variable로 치환 되는 건 넘기고, 없는 건 쿼리 파라미터로 전달
+        // ex) http://localhost:8080/basic/items/4?status=true
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}"; // redirectAtrribute 사용하면 기본적 인코딩 등 해결됨
     }
 
     @GetMapping("/{itemId}/edit")
