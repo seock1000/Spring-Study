922abf8beb90296b6344c82111bc837df4f04aba
seock1000 <tjrcjs0705@gmail.com>
2024-03-29 19:33:57 +0900

    스프링 MVC 2 - 수정에 Baen Validation 적용

--- spring-web-core-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ spring-web-core-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -74,8 +74,23 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
+    // 상품 수정에 validation 적용
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //특정 필드 예외가 아닌 전체 예외
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+        //검증에 실패하면(에러가 있으면) 다시 수정 폼으로 이동
+        if(bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
