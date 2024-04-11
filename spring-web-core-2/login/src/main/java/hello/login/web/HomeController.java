ec56555f246d92619cacdc9bcb86cce38415aea1
seock1000 <tjrcjs0705@gmail.com>
2024-04-11 19:42:55 +0900

    스프링 MVC 2 - ArgumentResolver 활용

--- spring-web-core-2/login/src/main/java/hello/login/web/HomeController.java
+++ spring-web-core-2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -80,8 +81,21 @@ public class HomeController {
 
     // 세션 확인 로직을 @SessionAttribute가 제공
     // 세션 생성은 제공하지 않음
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 home
+        if(loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
 
         // 세션에 회원 데이터가 없으면 home
         if(loginMember == null) {
