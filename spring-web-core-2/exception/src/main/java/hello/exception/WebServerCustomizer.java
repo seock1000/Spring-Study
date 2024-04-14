b6c4c74ca2cd4692090ba943dc6d31d4b6dcb9bf
seock1000 <tjrcjs0705@gmail.com>
2024-04-14 16:02:17 +0900

    스프링 MVC 2 - 서블릿 에러페이지 등록

---
+++ spring-web-core-2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,23 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        // 각 에러 발생시 이동 경로 지정
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        // RuntimeException의 자식 예외까지 처리
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        // 등록
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
